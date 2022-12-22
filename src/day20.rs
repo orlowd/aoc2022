624818d0f9c19384a4ba1645ba8a31b0e85c1e14
Denis Orlov <denorl2009@gmail.com>
2022-12-23 00:54:21 +0300

    day20: add solution

---
+++ src/day20.rs
@@ -0,0 +1,64 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
+
+fn mix_numbers(numbers: &Vec<i64>, times: u8) -> Vec<usize> {
+    let mut indexes: Vec<usize> = (0..numbers.len()).collect();
+
+    for _ in 0..times {
+        for (i, &num) in numbers.iter().enumerate() {
+            let old_pos = indexes.iter()
+                .position(|&index| index == i).unwrap();
+
+            indexes.remove(old_pos);
+
+            let new_pos = (old_pos as i64 + num as i64)
+                .rem_euclid(indexes.len() as i64) as usize;
+            indexes.insert(new_pos, i);
+        }
+    }
+
+    indexes
+}
+
+fn grove_coords(numbers: &Vec<i64>, indexes: &Vec<usize>) -> i64 {
+    let zero_pos = numbers.iter().position(|&i| i == 0).unwrap();
+    let zero_idx = indexes.iter().position(|&i| i == zero_pos).unwrap();
+
+    [1000, 2000, 3000].into_iter().map(|idx| {
+        let idx = (idx + zero_idx) % numbers.len();
+
+        numbers[indexes[idx]]
+    }).sum()
+}
+
+fn main() {
+    let path = Path::new("src/day20.txt");
+    let reader = match File::open(path) {
+        Err(e) => panic!("could not open input file at {}: {}", path.display(), e),
+        Ok(file) => BufReader::new(file),
+    };
+
+    let mut numbers: Vec<i64> = reader
+        .lines()
+        .map(|l| l.unwrap().parse().unwrap())
+        .collect();
+
+    let indexes = mix_numbers(&numbers, 1);
+    let sum = grove_coords(&numbers, &indexes);
+
+    println!("[Part 1] The sum of the three numbers that form the \
+              grove coordinates is {}", sum);
+
+    const DECRYPTION_KEY: i64 = 811589153;
+    const ROUNDS: u8 = 10;
+
+    numbers.iter_mut().for_each(|num| *num *= DECRYPTION_KEY );
+
+    let indexes = mix_numbers(&numbers, ROUNDS);
+    let sum = grove_coords(&numbers, &indexes);
+
+    println!("[Part 2] The sum of the three numbers that form the \
+              grove coordinates is actually {}", sum);
+}
